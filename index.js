2604424c4ec04847d93787d7056a935754e49408
aditya2272sharma <aditya.2272.sharma@gmail.com>
2017-05-21 00:20:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-var http = require('http');
-var port = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
 
-http.createServer(function(req, res){
-  res.statusCode = 200;
-  res.setEncoding('utf8');
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-}).listen(port);
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
